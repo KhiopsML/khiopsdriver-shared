bf5822dd85c19b0d3d9106b35849d12e95258916
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-03-18 13:23:33 +0100

    Add handy shell aliases

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Khiops storage driver shared components
 
-This repository is dedicated to host components useful for many or all [Khiops] drivers.
+This repository is dedicated to host components useful for many or all [Khiops](https://github.com/KhiopsML) drivers.
 
-Since the storage drivers must conform to [a common interface][interface], there is a single integration test suite [here][integrationtests].
+Since the storage drivers must conform to [a common interface](./integrationtests/plugin.hpp), there is a single integration test suite [here](./integrationtests/).
 
 
-[Khiops]: https://github.com/KhiopsML
-[interface]: ./integrationtests/plugin.hpp
-[integrationtests]: ./integrationtests/
+# Working on the drivers
+
+See [here](./handy_aliases.sh) for shell aliases that may be useful when working on the drivers.
---
+++ handy_aliases.sh
@@ -0,0 +1,27 @@
+# Generate build system for debug configuration with tests enabled
+alias khiopsdrivergendbg='cmake --preset ninja-dbg -DBUILD_TESTS=ON'
+
+# Build using debug configuration
+alias khiopsdriverbuilddbg='cmake --build --preset ninja-dbg -j'
+
+# Run tests using debug configuration, stopping on the first failure
+alias khiopsdrivertestdbg='ctest --preset ninja-dbg --stop-on-failure'
+
+# S3 driver aliases
+alias s3gendbg='khiopsdrivergendbg'
+alias s3builddbg='khiopsdriverbuilddbg'
+alias s3testdbg='STORAGE_DRIVER_TEST_URL_PREFIX=s3://diod-data-di-jupyterhub khiopsdrivertestdbg'
+
+# GCS driver aliases
+alias gcsgendbg='khiopsdrivergendbg'
+alias gcsbuilddbg='khiopsdriverbuilddbg'
+alias gcstestdbg='STORAGE_DRIVER_TEST_URL_PREFIX=gs://data-test-khiops-driver-gcs'
+
+# Azure driver aliases
+alias azgendbg='khiopsdrivergendbg'
+alias azbuilddbg='khiopsdriverbuilddbg'
+alias azetestdbg="AZURE_EMULATED_STORAGE=true \
+    AZURE_STORAGE_CONNECTION_STRING='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;' \
+    STORAGE_DRIVER_TEST_URL_PREFIX=http://localhost:10000/devstoreaccount1/data-test-khiops-driver-azure khiopsdrivertestdbg"
+alias azbtestdbg='STORAGE_DRIVER_TEST_URL_PREFIX=https://khiopsdriverazure.blob.core.windows.net/data-test-khiops-driver-azure khiopsdrivertestdbg'
+alias azftestdbg='STORAGE_DRIVER_TEST_URL_PREFIX=https://khiopsdriverazure.file.core.windows.net/data-test-khiops-driver-azure khiopsdrivertestdbg'
