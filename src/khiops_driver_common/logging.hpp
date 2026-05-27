ef072976e136719718745e5f4c1fa432600edc8c
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-05-27 13:46:09 +0200

    Remove duplicate declaration

--- src/khiops_driver_common/logging.hpp
+++ src/khiops_driver_common/logging.hpp
@@ -7,8 +7,6 @@ namespace khiops_driver_common {
 
 spdlog::logger *GetLogger(const std::string &logger_name, const std::string &logfile_envvar_name, const std::string &loglevel_envvar_name);
 
-spdlog::logger *GetLogger();
-
 std::string GetLastError();
 
 } // namespace khiops_driver_common
