b4977dce0f1eddf5849fff8a0f5a6494a0b8fde7
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-06-08 13:45:01 +0200

    Add comments

--- src/khiops_driver_common/util.hpp
+++ src/khiops_driver_common/util.hpp
@@ -20,26 +20,38 @@ A collection of utilities.
 
 namespace khiops_driver_common {
 
+// A unique pointer to "void" with a custom deleter.
 using CustomVoidUniquePtr = std::unique_ptr<void, void (*)(void *)>;
 
+// Split string using a delimiter character.
 std::vector<std::string> Split(const std::string &str, char delim, long long int nMaxSplits = -1, bool bRemoveEmpty = false);
+// Check if string has a given prefix.
 bool StartsWith(const std::string &str, const std::string &prefix);
+// Check if string has a given suffix.
 bool EndsWith(const std::string &str, const std::string &suffix);
+// Get a new string which is a lowercase version of the given string.
 std::string ToLower(const std::string &str);
 
+// Randomly return true or false.
 bool RandomBool();
 
+// Get environment variable, returning an empty string if not found.
 std::string GetEnvVar(const std::string &sVarName, bool bForbidLogging = false);
+// Get environment variable, returning a given default value if not found.
 std::string GetEnvVarOrDefault(const std::string &sVarName, const std::string &sDefaultValue, bool bForbidLogging = false);
 
+// Find the position of the first globbing character inside a string.
 size_t FindGlobbingChar(const std::string &str);
+// Test if a string contains globbing characters.
 bool IsGlobbingPattern(const std::string &str);
+// Check that a string does not contain globbing characters.
 int CheckIsNotGlobbingPattern(const std::string &str);
 
-// Function to detect if an URL points to a directory.
+// Detect if a URL points to a directory.
 bool IsDirUrl(const std::string &url);
 
-struct FileReader;  // Forward declaration
+struct FileReader;
+// Read a given number of bytes from a file reader to a buffer.
 int FRead(size_t *result, void *ptr, FileReader *file_reader, size_t size, size_t count);
 
 } // namespace khiops_driver_common
